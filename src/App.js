fd6d37f0b2d868ad736f8d2ed078c1081d3cb92f
TrungLV <vmo-vie002l@prestigein.com-at-313916016233>
2021-12-15 14:50:13 +0700

    update code

---
+++ src/App.js
@@ -0,0 +1,39 @@
+﻿import React from 'react';
+import FacebookLogin from 'react-facebook-login';
+import GoogleLogin from 'react-google-login';
+
+function App() {
+
+  const responseGoogle = (response) => {
+    console.log(response);
+  }
+  const responseFacebook = (response) => {
+    console.log(response);
+  }
+
+  return (
+    <>
+    <div style={{width: '200px'}}>
+<GoogleLogin
+    clientId="465688552574-ind2q43ht8g6q3va7rclkr6j2oph15bk.apps.googleusercontent.com"
+    buttonText="Login"
+    onSuccess={responseGoogle}
+    onFailure={responseGoogle}
+    cookiePolicy={'single_host_origin'}
+    style={{width: '100%'}}
+  />
+   
+    </div>
+    <div>
+      <FacebookLogin
+    appId="366827301115029"
+    autoLoad={true}
+    fields="name,email,picture"
+    callback={responseFacebook} />
+    </div>
+    </>
+    
+  );
+}
+
+export default App;
